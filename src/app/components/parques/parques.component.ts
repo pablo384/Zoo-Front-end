4d4f69ae826f0d43b091b6d827c9c08dea61e443
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-04 19:21:40 -0400

    NgDocheck

--- src/app/components/parques/parques.component.ts
+++ src/app/components/parques/parques.component.ts
@@ -1,6 +1,6 @@
 import {
   Component, Input, Output, EventEmitter,
-  OnChanges, SimpleChanges, OnInit
+  OnChanges, SimpleChanges, OnInit, DoCheck
 } from '@angular/core';
 
 @Component({
@@ -9,7 +9,7 @@ import {
   styleUrls: ['parques.component.css']
 })
 
-export class ParquesComponent implements OnChanges, OnInit {
+export class ParquesComponent implements OnChanges, OnInit, DoCheck {
 
 
   @Input() nombre: String;
@@ -27,17 +27,25 @@ export class ParquesComponent implements OnChanges, OnInit {
   }
 
   //ciclos de vida de los componentes
+
+  //se ejecuta siempre que se le hacen cambios a un componente
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Existen cambios en las propiedades');
   }
 
+  //siempre que se inicie el componente, Una sola vez
   ngOnInit(): void {
     console.log('Meotodo OnInit Lanzado');
   }
 
+  //siempre que se ejecuta un cambio en la aplicacion por ejemplo rutas, cambio/evento en angular,
+  // --despues de iniciar el componente
+  ngDoCheck(): void {
+    console.log("Esto es doCHeck")
+  }
+
   emitirEvento() {
     this.pasameDatos.emit({
-
       'nombre': this.nombre,
       'metros': this.metros,
       'vegetacion': this.vegetacion,
