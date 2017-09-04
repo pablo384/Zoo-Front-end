51e412bdbc2ea45360017bfd5f0cd19b525ca04d
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-04 18:57:27 -0400

    Comunicacion entre componentes Inputs y Outputs

--- src/app/components/parques/parques.component.ts
+++ src/app/components/parques/parques.component.ts
@@ -1,22 +1,36 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
-	selector:'parques',
-	templateUrl:'parques.component.html',
-	styleUrls:['parques.component.css']
+  selector: 'parques',
+  templateUrl: 'parques.component.html',
+  styleUrls: ['parques.component.css']
 })
 
 export class ParquesComponent {
 
-	@Input() nombre:String;
-	@Input('metros_c') metros:number;
-	public vegetacion:String
-	public abierto:boolean;
+  @Input() nombre: String;
+  @Input('metros_c') metros: number;
+  public vegetacion: String;
+  public abierto: boolean;
 
-	constructor(){
-		this.nombre='Parque natural para caballos';
-		this.metros=450;
-		this.vegetacion='Atlas';
-		this.abierto=true;
-	}
+  @Output() pasameDatos = new EventEmitter();
+
+  constructor() {
+    this.nombre = 'Parque natural para caballos';
+    this.metros = 450;
+    this.vegetacion = 'Atlas';
+    this.abierto = true;
+  }
+
+  emitirEvento() {
+    this.pasameDatos.emit({
+
+      'nombre': this.nombre,
+      'metros': this.metros,
+      'vegetacion': this.vegetacion,
+      'abierto': this.abierto
+
+    });
+
+  }
 }
--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,21 +1,27 @@
 import {Component} from '@angular/core';
 
 @Component({
-	selector:'tienda',
-	templateUrl:'tienda.component.html',
-	styleUrls:['tienda.component.css']
+  selector: 'tienda',
+  templateUrl: 'tienda.component.html',
+  styleUrls: ['tienda.component.css']
 })
 
 export class TiendaComponent {
 
-	public titulo;
-	public nombreDelparque: string;
+  public titulo;
+  public nombreDelparque: string;
+  public miParque;
 
-	constructor(){
-		this.titulo='Esta es la tienda';
-	}
+  constructor() {
+    this.titulo = 'Esta es la tienda';
+  }
 
-	mostrarNombre(){
-		console.log(this.nombreDelparque);
-	}
+  mostrarNombre() {
+    console.log(this.nombreDelparque);
+  }
+
+  verDatosParque(event) {
+    console.log(event);
+    this.miParque=event;
+  }
 }
