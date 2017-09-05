817b67ed54269711b12e146a421cd09b010c8a91
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-04 22:15:43 -0400

    maquetacion de home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit{
-  title="Home";
+  title="Bienvenido a NGZOO";
 
   ngOnInit(): void {
     console.log('home.component.ts')
