fa2a2b63c86a0513bd84b2e2c2bd1bf68014661c
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-04 20:15:26 -0400

    LocalStorage y DoCheck. Ejemplo Practico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, DoCheck{
+
+  title = 'Curso de Angular 4 Avanzado';
+  emailContacto:string;
+
+  ngOnInit(): void {
+    this.emailContacto=localStorage.getItem('emailContacto')
+    //console.log(localStorage.getItem('emailContacto'))
+  }
+
+
+  ngDoCheck(): void {
+    this.emailContacto=localStorage.getItem('emailContacto')
+  }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit{
   title="Contacto";
+  emailContacto:string;
 
   ngOnInit(): void {
     console.log('contact.component.ts')
+  }
+  guardarEmail(){
+    //local storage
+    localStorage.setItem('emailContacto',this.emailContacto);
+    console.log(localStorage.getItem('emailContacto'))
+
   }
 }
