0f4d134a59104310719e9188caae6653b9c87562
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-11 16:59:42 -0400

    servicios para usuarios

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
+import {User} from '../../models/user'
+import {GLOBAL} from '../../services/global';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'register',
-  templateUrl: './register.component.html'
+  templateUrl: './register.component.html',
+  providers:[UserService]
 })
 export class RegisterComponent implements OnInit{
 
   public title:String;
+  public user:User;
 
   constructor(
     private _route:ActivatedRoute,
-    private _router:Router
+    private _router:Router,
+    private _userService:UserService
   ){
     this.title='Registro';
+    this.user=new User('','','','','','','');
   }
 
   ngOnInit(): void {
       console.log('register.component cargado!!');
+      console.log(this._userService.register());
+  }
+
+  onSubmit(){
+      console.log(this.user);
+
   }
 }
