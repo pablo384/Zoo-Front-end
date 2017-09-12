bc9e6d989130c354d7573da18a551dfcde29df6a
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-12 17:33:17 -0400

    Componente agragr animales

--- src/app/admin/components/add/add.component.ts
+++ src/app/admin/components/add/add.component.ts
@@ -1,10 +1,40 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {GLOBAL} from '../../../services/global';
+import {Animal} from '../../../models/animal';
+import {AnimalService} from '../../../services/animal.service';
+import {UploadService} from '../../../services/upload.service';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'admin-add',
-  templateUrl: './add.component.html'
+  templateUrl: './add.component.html',
+  providers:[UserService, AnimalService, UploadService]
 })
-export class AddComponent{
+export class AddComponent implements OnInit{
 
-  title = 'Agregar Animal';
+  public title;
+  public animal:Animal;
+  public identity;
+  public token;
+  public url:string;
+
+  constructor(
+    private _animalService:AnimalService,
+    private _route:ActivatedRoute,
+    private _router:Router,
+    private _userService:UserService,
+    private _uploadService:UploadService
+  ){
+    this.title='Agregar Animal';
+    this.animal=new Animal('','',2017,'','');
+    this.identity=this._userService.getIdentity();
+    this.token=this._userService.getToken();
+    this.url=GLOBAL.url;
+  }
+
+
+  ngOnInit(): void {
+      console.log('animal-add componente ha sido cargado!!')
+  }
 }
---
+++ src/app/services/animal.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {GLOBAL} from './global';
+
+
+@Injectable()
+export class AnimalService {
+  public url: string;
+
+  constructor(private _http: Http) {
+    this.url = GLOBAL.url;
+  }
+
+  addAnimal(token, animal){
+
+    let params = JSON.stringify(animal);
+    let headers=new Headers({
+      'Content-Type':'application/json',
+      'Authorization':token
+    });
+
+    return this._http.post(this.url+'animal',params, {headers:headers})
+      .map(res => res.json());
+
+  }
+
+}
