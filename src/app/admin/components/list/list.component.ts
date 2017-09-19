ba302f2dea60b16606b893e590caa4d35253316b
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-19 18:56:29 -0400

    Boton y popup de borrado de animales

--- src/app/admin/components/list/list.component.ts
+++ src/app/admin/components/list/list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Animal} from '../../../models/animal';
 import {AnimalService} from '../../../services/animal.service';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'admin-list',
@@ -13,17 +14,24 @@ export class ListComponent implements OnInit{
 
   public title;
   public animals: Animal[];
+  public token;
 
   constructor(private _animalService: AnimalService,
+              private _userService:UserService,
               private _route: ActivatedRoute,
               private _router: Router
   ) {
 
     this.title = 'Listado de Animales';
+    this.token=this._userService.getToken();
   }
 
 
   ngOnInit(): void {
+    this.getAnimals();
+  }
+
+  getAnimals(){
     this._animalService.getAnimals().subscribe(
       response=>{
         if (!response.animals){
@@ -38,6 +46,22 @@ export class ListComponent implements OnInit{
         if (erms != null) {
 
         }
+      }
+    )
+  }
+  deleteAnimal(id){
+    $('#myModal-'+id).modal('hide');
+    this._animalService.deleteAnimal(this.token,id).subscribe(
+      response =>{
+        if (!response.animal){
+          alert('Error en el Servidor');
+        }else {
+
+        }
+        this.getAnimals();
+      },
+      error =>{
+        alert('Error en el Servidor');
       }
     )
   }
--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Headers, Http} from '@angular/http';
+import {Headers, Http, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {GLOBAL} from './global';
 
@@ -43,6 +43,17 @@ export class AnimalService {
       'Authorization':token
     });
     return this._http.put(this.url+'animal/'+id,params,{headers:headers})
+      .map(res => res.json());
+  }
+
+  deleteAnimal(token, id){
+    let headers=new Headers({
+      'Content-Type':'application/json',
+      'Authorization':token
+    });
+
+    let options = new RequestOptions({headers:headers});
+    return this._http.delete(this.url+'animal/'+id,options)
       .map(res => res.json());
   }
 
